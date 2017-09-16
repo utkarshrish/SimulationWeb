e7529f130f3ff0e71ea90c67477cbfdf2de64cc9
utkarshrish <utk.rish21@gmail.com>
2017-09-16 11:46:22 +0530

    using production build

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,11 @@ module.exports = {
     },
 
     plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        }),
         new webpack.optimize.UglifyJsPlugin({
             beautify: false,
             mangle: {
